00dfe78656a3bece6b238ab53bfe4d0e022b8500
CLIFF <k023c0018@g.neec.ac.jp>
2025-02-04 11:50:35 +0000

    プロジェクトコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/new'
+  get 'projects/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
