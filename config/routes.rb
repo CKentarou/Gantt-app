a642afbeb7ef87bf7123a638d3b6a4944555e731
CLIFF <k023c0018@g.neec.ac.jp>
2025-02-05 06:09:47 +0000

    ルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-  get 'projects/new'
-  get 'projects/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "projects#index"
+  
+  resources :projects do
+    member do
+      get :gantt
+    end
+  end
+  
+  scope '/api' do
+    get "/projects/:project_id/data", to: "gantt#data"
+    
+    post "/projects/:project_id/task", to: "tasks#add"
+    put "/projects/:project_id/task/:id", to: "tasks#update"
+    delete "/projects/:project_id/task/:id", to: "tasks#delete"
+
+    post "/projects/:project_id/link", to: "links#add"
+    put "/projects/:project_id/link/:id", to: "links#update"
+    delete "/projects/:project_id/link/:id", to: "links#delete"
+  end
 end
