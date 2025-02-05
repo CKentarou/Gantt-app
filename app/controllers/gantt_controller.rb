2c5700bc5943a3b286cd1575f9b57495d146a4af
CLIFF <k023c0018@g.neec.ac.jp>
2025-02-05 06:07:29 +0000

    ガントコントローラ機能追加

--- app/controllers/gantt_controller.rb
+++ app/controllers/gantt_controller.rb
@@ -1,2 +1,31 @@
 class GanttController < ApplicationController
+  def data
+    project = Project.find(params[:project_id])
+    tasks = project.tasks
+    task_ids = tasks.pluck(:id)
+    
+    links = Link.where(source: task_ids).or(Link.where(target: task_ids))
+
+    render json: {
+      data: tasks.map { |task| 
+        {
+          id: task.id,
+          text: task.text,
+          start_date: task.start_date.to_formatted_s(:db),
+          duration: task.duration,
+          progress: task.progress,
+          parent: task.parent,
+          open: true
+        }
+      },
+      links: links.map { |link|
+        {
+          id: link.id,
+          source: link.source,
+          target: link.target,
+          type: link.link_type
+        }
+      }
+    }
+  end
 end
