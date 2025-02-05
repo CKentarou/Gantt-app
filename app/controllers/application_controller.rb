392258622f6b89ea0b253c6d860b6f0ab406000c
CLIFF <k023c0018@g.neec.ac.jp>
2025-02-05 06:59:52 +0000

    ユーザ登録時に:nameの受け取りを許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configured_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top, :about]
+
+  protected
+
+  def configured_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
