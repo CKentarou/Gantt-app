14878c8424300ccf131c8fa3b6ff0dea0c1df56a
CLIFF <k023c0018@g.neec.ac.jp>
2025-02-06 01:02:11 +0000

    外部制約追加

--- db/migrate/20250206005954_create_project_users.rb
+++ db/migrate/20250206005954_create_project_users.rb
@@ -3,9 +3,11 @@ class CreateProjectUsers < ActiveRecord::Migration[6.1]
     create_table :project_users do |t|
       t.references :project, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.string :role
+      t.string :role, default: 'member'
 
       t.timestamps
     end
+
+    add_index :project_users, [:project_id, :user_id], unique: true
   end
 end
